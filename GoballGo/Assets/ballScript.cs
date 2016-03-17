9fb8db7fa2649858aa02f39d827568e3ee477eb5
iodandidiot <iodandidiot@gmail.com>
2016-03-17 23:25:33 +0200

    first

---
+++ GoballGo/Assets/ballScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballScript : MonoBehaviour {
+
+    bool big,small=false;
+    bool normal = true;
+	// Use this for initialization
+	void Start () 
+    {
+        Rigidbody2D rigid = gameObject.GetComponent<Rigidbody2D>();
+        rigid.AddTorque(-180);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //Rigidbody2D rigid = gameObject.GetComponent<Rigidbody2D>();
+        //rigid.AddTorque(-180);
+	}
+
+    public void ScaleBig()
+    {
+        if (normal)
+        {
+            big = true;
+            normal = false;
+            gameObject.transform.localScale = new Vector2(1.25f, 1.25f);
+            Rigidbody2D rigid = gameObject.GetComponent<Rigidbody2D>();
+            rigid.AddTorque(-10);
+        }
+        if (small)
+        {
+            gameObject.transform.localScale = new Vector2(1, 1);
+            small = false;
+            normal = true;
+        }        
+    }
+    public void ScaleSmall()
+    {
+        if (normal)
+        {
+            small = true;
+            normal = false;
+            gameObject.transform.localScale = new Vector2(0.75f, 0.75f);
+        }
+        if (big)
+        {
+            gameObject.transform.localScale = new Vector2(1, 1);
+            big = false;
+            normal = true;
+        }
+
+    }
+}
